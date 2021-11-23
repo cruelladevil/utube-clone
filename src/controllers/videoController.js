ed57f6176de43296c31e27325da924c9ecab5023
CruellaDeVil <dev.timetravel@gmail.com>
2021-11-23 17:00:46 +0900

    made video-upload template

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -41,13 +41,28 @@ export const getEditVideo = (req, res) => {
   const { id } = req.params;
   const video = videos[id - 1];
   return res.render("edit", { pageTitle: "Edit", fakeUser, video });
-}
+};
 export const postEditVideo = (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
   videos[id - 1].title = title;
   return res.redirect(`/videos/${id}`);
-}
+};
 export const searchVideo = (req, res) => res.send("Search");
 export const deleteVideo = (req, res) => res.send("Delete Video");
-export const uploadVideo = (req, res) => res.send("Upload Video");
+export const getUploadVideo = (req, res) => {
+  res.render("upload", { pageTitle: "Upload Video", fakeUser });
+};
+export const postUploadVideo = (req, res) => {
+  const { title } = req.body;
+  const newVideo = {
+    title,
+    rating: 0,
+    comments: 0,
+    createdAt: "just now",
+    views: 0,
+    id: videos.length + 1,
+  }
+  videos.push(newVideo);
+  return res.redirect("/");
+};
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { watchVideo, getEditVideo, postEditVideo, deleteVideo, uploadVideo } from "../controllers/videoController";
+import { watchVideo, getEditVideo, postEditVideo, deleteVideo, getUploadVideo, postUploadVideo } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/:id(\\d+)", watchVideo);
 videoRouter.route("/:id(\\d+)/edit").get(getEditVideo).post(postEditVideo);
 videoRouter.get("/:id(\\d+)/delete", deleteVideo);
-videoRouter.get("/upload", uploadVideo);
+videoRouter.route("/upload").get(getUploadVideo).post(postUploadVideo);
 
 export default videoRouter;
