972d8fa99d23eb33cb256a9539a73c223dc90297
CruellaDeVil <dev.timetravel@gmail.com>
2021-12-01 20:54:07 +0900

    email/username validation and password confirmation

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,7 +2,21 @@ import userModel from "../model/User";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 export const postJoin = async (req, res) => {
-  const { name, email, username, password, location } = req.body;
+  const { name, email, username, password, password2, location } = req.body;
+  const pageTitle = "Join";
+  if (password !== password2) {
+    return res.render("join", {
+      pageTitle,
+      errorMessage: "check password confirmation",
+    });
+  }
+  const exists = await userModel.exists({ $or: [{ email }, { username }] });
+  if (exists) {
+    return res.render("join", {
+      pageTitle,
+      errorMessage: "this email/username is already taken.",
+    });
+  }
   userModel.create({
     name,
     email,
