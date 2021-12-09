8effd5481457e194d25168b88039c72b05849d65
CruellaDeVil <dev.timetravel@gmail.com>
2021-12-09 23:54:01 +0900

    protector and public middlewares

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { getJoin, postJoin, getLogin, postLogin } from "../controllers/userController";
 import { recommendedVideo, searchVideo } from "../controllers/videoController";
+import { publicOnlyMiddleware } from "../middlewares";
 
 const globalRouter = express.Router();
 
 globalRouter.get("/", recommendedVideo);
-globalRouter.route("/join").get(getJoin).post(postJoin);
-globalRouter.route("/login").get(getLogin).post(postLogin);
+globalRouter.route("/join").all(publicOnlyMiddleware).get(getJoin).post(postJoin);
+globalRouter.route("/login").all(publicOnlyMiddleware).get(getLogin).post(postLogin);
 globalRouter.get("/search", searchVideo);
 
 export default globalRouter;
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { getEditUser, postEditUser, deleteUser, logout, seeUser } from "../controllers/userController";
+import { protectorMiddleware } from "../middlewares";
 
 const userRouter = express.Router();
 
-userRouter.route("/edit").get(getEditUser).post(postEditUser);
-userRouter.get("/delete", deleteUser);
-userRouter.get("/logout", logout);
-userRouter.get("/:id", seeUser);
+userRouter.route("/edit").all(protectorMiddleware).get(getEditUser).post(postEditUser);
+userRouter.get("/delete", protectorMiddleware, deleteUser);
+userRouter.get("/logout", protectorMiddleware, logout);
+userRouter.get("/:id", protectorMiddleware, seeUser);
 
 export default userRouter;
